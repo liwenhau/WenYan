b753382d595622797b06ac069db085f703be2955
liwenhau <1465041302@qq.com>
2022-02-12 16:33:59 +0800

    ✨ 引入赫蹏排版引擎

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,30 @@
 <template>
     <div class="flex flex-col md:flex-row">
-        <header class="">logo</header>
+        <header class="w-full h-full md:w-16 bg-fuchsia-800">logo</header>
         <div
             class="mx-auto w-full h-full p-2 shadow-md rounded-md bg-gradient-to-r from-gray-800 to-indigo-900 dark:bg-black text-blue-50"
         >
-            <div>作者</div>
-            <div>内容</div>
+            <div class="heti heti--ancient">
+                <h2>
+                    一剪梅·红藕香残玉簟秋
+                    <span class="heti-meta heti-small">
+                        [宋]
+                        <abbr title="号易安居士">李清照</abbr>
+                    </span>
+                </h2>
+                <p class="heti-verse heti-x-large">
+                    红藕香残玉簟秋。轻解罗裳，独上兰舟
+                    <span class="heti-hang">。</span>
+                    <br />云中谁寄锦书来，雁字回时，月满西楼
+                    <span class="heti-hang">。</span>
+                    <br />花自飘零水自流。一种相思，两处闲愁
+                    <span class="heti-hang">。</span>
+                    <br />此情无计可消除，才下眉头，却上心头
+                    <span class="heti-hang">。</span>
+                </p>
+            </div>
         </div>
-        <footer>点赞评论</footer>
+        <footer class="w-full h-full md:w-16 bg-fuchsia-800">点赞评论</footer>
     </div>
 </template>
 <script lang="ts">
