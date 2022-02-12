9f4c4affe922d57760ab814f6d5fcb0dd5c46f6b
liwenhau <1465041302@qq.com>
2022-02-12 10:25:53 +0800

    ✨ 自动引入图标

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,4 +1,14 @@
 <template>
+    <div class="flex flex-col md:flex-row">
+        <header class="">logo</header>
+        <div
+            class="mx-auto w-full h-full p-2 shadow-md rounded-md bg-gradient-to-r from-gray-800 to-indigo-900 dark:bg-black text-blue-50"
+        >
+            <div>作者</div>
+            <div>内容</div>
+        </div>
+        <footer>点赞评论</footer>
+    </div>
 </template>
 <script lang="ts">
 import { computed, defineComponent, unref, ref, reactive, toRefs } from 'vue';
@@ -7,9 +17,9 @@ export default defineComponent({
     setup() {
         const router = useRouter();
         const state = reactive({
-             name:'hhhh'
+            name: 'hhhh'
         });
-        return { ...toRefs(state)};
+        return { ...toRefs(state) };
     },
 });
 </script>
