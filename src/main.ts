8d8af49a5a3807f9992af781d61927dc3d9e672e
liwenhau <1465041302@qq.com>
2022-02-13 17:52:55 +0800

    ✨ 删除旧版图标库，引入naive-ui框架

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
-createApp(App).use(router).mount('#app')
+import {
+    // create naive ui
+    create,
+    NCard,
+    NIcon,
+    NIconWrapper,
+    NBadge 
+    // component
+} from 'naive-ui'
+const naive = create({
+    components: [NCard,NIcon,NIconWrapper,NBadge]
+})
+createApp(App).use(router).use(naive).mount('#app')
